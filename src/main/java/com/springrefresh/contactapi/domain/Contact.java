93a1c004619f09dadae92647f596beced1ff5481
KaylaK1 <44605348+KaylaK1@users.noreply.github.com>
2024-06-26 18:28:58 -0400

    initial contact domain complete

--- src/main/java/com/springrefresh/contactapi/domain/Contact.java
+++ src/main/java/com/springrefresh/contactapi/domain/Contact.java
@@ -1,6 +1,30 @@
 package com.springrefresh.contactapi.domain;
 
+import org.hibernate.annotations.UuidGenerator;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_DEFAULT;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(NON_DEFAULT)
+@Table(name = "contacts")
 public class Contact {
+    @Id
+    @UuidGenerator
+    @Column(name = "id", unique = true, updatable = false)
     private String id;
     private String name;
     private String email;
