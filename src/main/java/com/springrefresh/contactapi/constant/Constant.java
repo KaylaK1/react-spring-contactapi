4ba53c26603d86b9ddc89b05a460c92134dfc6f7
KaylaK1 <44605348+KaylaK1@users.noreply.github.com>
2024-06-27 14:43:40 -0400

    added photoFunction and fileExtension to save images

---
+++ src/main/java/com/springrefresh/contactapi/constant/Constant.java
@@ -0,0 +1,6 @@
+package com.springrefresh.contactapi.constant;
+
+public class Constant {
+    // gets the users home directory
+    public static final String PHOTO_DIRECTORY = System.getProperty("user.home") + "/downloads/uploads";
+}
--- src/main/java/com/springrefresh/contactapi/repo/ContactRepo.java
+++ src/main/java/com/springrefresh/contactapi/repo/ContactRepo.java
@@ -2,8 +2,9 @@ package com.springrefresh.contactapi.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.springrefresh.contactapi.domain.Contact;
+
+import java.util.Optional;
 
 @Repository
 public interface ContactRepo extends JpaRepository<Contact, String>{
