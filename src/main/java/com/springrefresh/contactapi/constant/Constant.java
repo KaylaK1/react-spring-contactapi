00c61aa210e151d7465682f35ccc31696064a853
KaylaK1 <44605348+KaylaK1@users.noreply.github.com>
2024-07-27 17:27:47 -0400

    added cors configuration

--- src/main/java/com/springrefresh/contactapi/constant/Constant.java
+++ src/main/java/com/springrefresh/contactapi/constant/Constant.java
@@ -3,4 +3,5 @@ package com.springrefresh.contactapi.constant;
 public class Constant {
     // gets the users home directory
     public static final String PHOTO_DIRECTORY = System.getProperty("user.home") + "/downloads/uploads/";
+    public static final String X_REQUESTED_WITH = "X-Requested-With";
 }
